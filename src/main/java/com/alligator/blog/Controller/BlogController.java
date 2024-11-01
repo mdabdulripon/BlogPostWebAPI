c4d7814928827d86b19dd16c9c11bc6db0f5d383
Abdul Awal Ripon <aripon@Abduls-Mac-mini.local>
2024-11-01 09:12:24 -0500

    rename the package

--- src/main/java/com/alligator/blog/Controller/BlogController.java
+++ src/main/java/com/alligator/blog/Controller/BlogController.java
@@ -1,4 +1,4 @@
-package com.blog.Controller;
+package com.alligator.blog.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/test/java/com/alligator/blog/BlogWebApiApplicationTests.java
+++ src/test/java/com/alligator/blog/BlogWebApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.blog;
+package com.alligator.blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
